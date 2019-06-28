138b5037301c506f7408b278af5b815a956d1cab
Manos Kirtas <manolis.kirt@gmail.com>
2019-06-28 14:43:51 +0300

    Remove parameter & tweak a white space

--- addOns/websocket/src/main/zapHomeFiles/scripts/scripts/websocketpassive/base64_disclosure.js
+++ addOns/websocket/src/main/zapHomeFiles/scripts/scripts/websocketpassive/base64_disclosure.js
@@ -1,4 +1,4 @@
-// * This Community Script will analyze incoming websocket messages for base64 strings
+ // * This Community Script will analyze incoming websocket messages for base64 strings
 
 // * Regex Test: https://regex101.com/r/OOElRY/3
 // ** Forked by: https://regex101.com/library/dS0sM8
@@ -9,7 +9,7 @@
 // Right click the script in the Scripts tree and select "enable"  or "disable"
 
 OPCODE_TEXT = 0x1;
-RISK_INFO 	= 0;
+RISK_INFO = 0;
 CONFIDENCE_MEDIUM = 2;
 
 PRINT_RESULTS = false;
@@ -26,13 +26,12 @@ function scan(helper,msg) {
     }
     var message = String(msg.getReadablePayload()).valueOf();
     if( (matches = message.match(base64Regex)) != null ){
-        var counter = 0;
 
         matches.forEach(function(evidence){
 
             decodedEvidence = new JavaString(base64Decoder.decode(evidence));
             if(PRINT_RESULTS){
-                print("Message: " + message); 
+                print("Message: " + message);
                 print("Evidence: " + evidence);
                 print("Decoded Evidence: " + decodedEvidence);
             }
@@ -43,7 +42,6 @@ function scan(helper,msg) {
                 .setDescription("A Base64-encoded string has been founded in the websocket incoming message. Base64-encoded data may contain sensitive" +
                                 "information such as usernames, passwords or cookies which should be further inspected. Decode evidence: "
                                 + decodedEvidence + ".")
-                .setParam(counter++)
                 .setSolution("Base64-encoding should not be used to store or send sensitive information.")
                 .setEvidence(evidence)
                 .raise();
